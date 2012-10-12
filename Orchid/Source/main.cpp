6065d8a48003d98108dc4a160131856f9989fc5d
Hypervariate <hypervariate@gmail.com>
2012-10-12 18:01:40 -0400

    added box2d test code to main

--- Orchid/Source/main.cpp
+++ Orchid/Source/main.cpp
@@ -28,8 +28,11 @@ void Deinitialize(){
 
 int main()
 {
-	
+	//--------------------------------------------------------------------------
 	Initialize();
+    //--------------------------------------------------------------------------
+
+    bool doSleep = true;
 
     // Define the gravity vector.
     b2Vec2 gravity(0.0f, -10.0f);
@@ -37,12 +40,49 @@ int main()
     // Construct a world object, which will hold and simulate the rigid bodies.
     b2World world(gravity);
 
-	BreakoutGame::Run();
+    b2BodyDef groundBodyDef;
+
+    groundBodyDef.position.Set(0.0f, -10.0f);
+
+    b2Body* groundBody = world.CreateBody(&groundBodyDef);
+
+    b2PolygonShape groundBox;
+
+    groundBox.SetAsBox(50.0f, 10.0f);
+
+    groundBody->CreateFixture(&groundBox, 0.0f);
+
+    b2BodyDef bodyDef;
+
+    bodyDef.type = b2_dynamicBody;
+
+    bodyDef.position.Set(0.0f, 4.0f);
+
+    b2Body* body = world.CreateBody(&bodyDef);
+
+    b2PolygonShape dynamicBox;
+
+    dynamicBox.SetAsBox(1.0f, 1.0f);
+
+    b2FixtureDef fixtureDef;
+
+    fixtureDef.shape = &dynamicBox;
+
+    fixtureDef.density = 1.0f;
+
+    fixtureDef.friction = 0.3f;
+
+	body->CreateFixture(&fixtureDef);
+
+    //--------------------------------------------------------------------------
+
+    BreakoutGame::Run();
 	//BalloonFightGame::Run();
 	//AnimalCrossingGame::Run();
-    	
+    
+    //--------------------------------------------------------------------------	
 	Deinitialize();
-	
+	//--------------------------------------------------------------------------
 
 	return 0;
 }
