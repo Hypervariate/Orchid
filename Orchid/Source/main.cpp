fe6af337088124cc145ce9f3fa08e0446d8761ee
Nate <hypervariate@gmail.com>
2012-10-14 02:19:58 -0400

    Graphical demo of a box changing position (not rotation yet) and responding to keyboard events.

--- Orchid/Source/main.cpp
+++ Orchid/Source/main.cpp
@@ -22,22 +22,117 @@ using bio::stream;
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
-#define PTM_RATIO 32
+#define PTM_RATIO 16
+
+static ALLEGRO_TIMER *frame_timer = NULL;
+static int frames;
+static int fps;
+static int seconds;
+
+static ALLEGRO_EVENT_QUEUE *eventQueue;
+static ALLEGRO_EVENT ev;
+static ALLEGRO_DISPLAY *display = NULL;	//Allegro Display Object (back-buffer)
+static b2World* world;
+static double deltaTime;			//current time - lastTick
+static double milliseconds = 0;
+static double lastFrameTimeStamp = 0;
 
 
-// This is a simple example of building and running a simulation
-// using Box2D. Here we create a large ground box and a small dynamic
-// box.
-// There are no graphics for this example. Box2D is meant to be used
-// with your rendering engine in your game engine.
 
+bool Initialize()
+{
+    //Allegro----------------------------------------------------------------------------------
+    al_init();
+    
+    al_install_keyboard();
+    al_install_mouse();
+    al_install_joystick();
+    
+    frame_timer = al_create_timer(1.0 / 60);
+    display = al_create_display(SCREEN_WIDTH, SCREEN_HEIGHT);
+    
+    eventQueue = al_create_event_queue();
+    al_register_event_source(eventQueue, al_get_mouse_event_source());
+    al_register_event_source(eventQueue, al_get_keyboard_event_source());
+    al_register_event_source(eventQueue, al_get_display_event_source(display));
+    al_register_event_source(eventQueue, al_get_timer_event_source(frame_timer));
+    
+    al_start_timer(frame_timer);
+    frames = 0;
+
+    
+    
+    
+    al_init_primitives_addon();
+    
+    
+    
+    return true;
+    
+}
+bool Deinitialize()
+{
+    al_shutdown_primitives_addon();
+    al_destroy_event_queue(eventQueue);
+    al_destroy_timer(frame_timer);
+    al_destroy_display(display);
+    
+    delete world;
+	world = NULL;
+    
+    return true;
+
+}
+bool Update()
+{
+    al_wait_for_event(eventQueue, &ev);
+
+	if(ev.type == ALLEGRO_EVENT_TIMER && al_is_event_queue_empty(eventQueue)) {
+        
+		
+		milliseconds = al_get_timer_count(frame_timer);
+		deltaTime = milliseconds - lastFrameTimeStamp;
+		seconds = (milliseconds)/100.0f;
+		lastFrameTimeStamp = al_get_timer_count(frame_timer);
+//		GraphicsCore::PrintToDisplay(Timer::GetDeltaTime() , 0, 24, "Arcade", 0, 255, 0);
+		
+		frames++;
+		fps = frames/(seconds + 1);	//frames / second
+        
+//		GraphicsCore::PrintToDisplay(fps , WIDTH - 48, 0, "Arcade", 0, 255, 0);
+		
+		//cout << Timer::GetDeltaTime() << endl;
+		//GraphicsCore::Update();
+        
+        
+        
+        al_flip_display();
+
+	}
+    //requires display event source registered
+	if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+	{
+        return false;   //denit and quit
+	}
+    else if(ev.type == ALLEGRO_EVENT_KEY_UP)
+	{
+        if(ev.keyboard.keycode == ALLEGRO_KEY_ESCAPE){
+				return false;
+		}
+	}
+    return true;
+
+}
 int main(int argc, char **argv)
 {
     
-    //Box2D----------------------------------------------------------------------------------
     B2_NOT_USED(argc);
     B2_NOT_USED(argv);
     
+    
+    Initialize();
+    
+    //Box2D----------------------------------------------------------------------------------
     // Define the gravity vector.
     b2Vec2 gravity(0.0f, -10.0f);
     
@@ -45,16 +140,18 @@ int main(int argc, char **argv)
     bool doSleep = true;
     
     // Construct a world object, which will hold and simulate the rigid bodies.
-    b2World world(gravity, doSleep);
+
+    world = new b2World(gravity, doSleep);
+    
     
     // Define the ground body.
     b2BodyDef groundBodyDef;
-    groundBodyDef.position.Set(0.0f, -10.0f);
+    groundBodyDef.position.Set(0.0f, -SCREEN_HEIGHT/PTM_RATIO );
     
     // Call the body factory which allocates memory for the ground body
     // from a pool and creates the ground box shape (also from a pool).
     // The body is also added to the world.
-    b2Body* groundBody = world.CreateBody(&groundBodyDef);
+    b2Body* groundBody = world->CreateBody(&groundBodyDef);
     
     // Define the ground box shape.
     b2PolygonShape groundBox;
@@ -68,12 +165,13 @@ int main(int argc, char **argv)
     // Define the dynamic body. We set its position and call the body factory.
     b2BodyDef bodyDef;
     bodyDef.type = b2_dynamicBody;
-    bodyDef.position.Set(0.0f, 4.0f);
-    b2Body* body = world.CreateBody(&bodyDef);
+    bodyDef.position.Set(SCREEN_WIDTH/2/PTM_RATIO, 1.0f);
+    b2Body* body = world->CreateBody(&bodyDef);
     
     // Define another box shape for our dynamic body.
     b2PolygonShape dynamicBox;
-    dynamicBox.SetAsBox(1.0f, 1.0f);
+    float boxSize = 2.0f;
+    dynamicBox.SetAsBox(boxSize, boxSize);
     
     // Define the dynamic body fixture.
     b2FixtureDef fixtureDef;
@@ -91,80 +189,66 @@ int main(int argc, char **argv)
     // Prepare for simulation. Typically we use a time step of 1/60 of a
     // second (60Hz) and 10 iterations. This provides a high quality simulation
     // in most game scenarios.
-    float32 timeStep = 1.0f / 60.0f;
     int32 velocityIterations = 6;
     int32 positionIterations = 2;
+    float32 timeStep = 1.0f / 60.0f;
+    // When the world destructor is called, all bodies and joints are freed. This can
+    // create orphaned pointers, so be careful about your world management.
+
+    //ALLEGRO----------------------------------------------------------------------------------
     
-    // This is our little game loop.
-    for (int32 i = 0; i < 60; ++i)
+    while(Update())
     {
+
         // Instruct the world to perform a single step of simulation.
         // It is generally best to keep the time step and iterations fixed.
-        world.Step(timeStep, velocityIterations, positionIterations);
+        world->Step(timeStep, velocityIterations, positionIterations);
         
         // Clear applied body forces. We didn't apply any forces, but you
         // should know about this function.
-        world.ClearForces();
+        world->ClearForces();
         
         // Now print the position and angle of the body.
         b2Vec2 position = body->GetPosition();
         float32 angle = body->GetAngle();
         
         printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
-    }
-    
-    // When the world destructor is called, all bodies and joints are freed. This can
-    // create orphaned pointers, so be careful about your world management.
 
-    //ALLEGRO----------------------------------------------------------------------------------
+        
+        al_clear_to_color(al_map_rgb(0,0,0));
+        
+        ALLEGRO_COLOR color_blue;
+        color_blue.b = 256;
+        
+        ALLEGRO_COLOR color_red;
+        color_red.r = 256;
+        
+        ALLEGRO_COLOR color_green;
+        color_green.g = 256;
+        
+        ALLEGRO_COLOR color_purple;
+        color_purple.r = 128;
+        color_purple.b = 128;
+        
+        float x = position.x;
+        float y = position.y;
+        
+        al_draw_rectangle(PTM_RATIO*(x - boxSize), -PTM_RATIO*(y - boxSize), PTM_RATIO*(x + boxSize), -PTM_RATIO*(y + boxSize), color_purple, 1);
+        
+    };
     
-    ALLEGRO_DISPLAY *display = NULL;
     
-    if(!al_init()) {
-        fprintf(stderr, "failed to initialize allegro!\n");
-        return -1;
-    }
     
-    display = al_create_display(640, 480);
-    if(!display) {
-        fprintf(stderr, "failed to create display!\n");
-        return -1;
-    }
-    
-    al_clear_to_color(al_map_rgb(0,0,0));
 
-    al_init_primitives_addon();
-    
-    ALLEGRO_COLOR color_blue;
-    color_blue.b = 256;
-    
-    ALLEGRO_COLOR color_red;
-    color_red.r = 256;
-    
-    ALLEGRO_COLOR color_green;
-    color_green.g = 256;
-    
-    ALLEGRO_COLOR color_purple;
-    color_purple.r = 128;
-    color_purple.b = 128;
     
     
-    /* blue vertical line */
-    al_draw_line(0.5, 0, 0.5, 6, color_blue, 1);
-    /* red horizontal line */
-    al_draw_line(2, 1, 6, 1, color_red, 2);
-    /* green filled rectangle */
-    al_draw_filled_rectangle(3, 4, SCREEN_WIDTH/4, SCREEN_HEIGHT/4, color_green);
-    /* purple outlined rectangle */
-    al_draw_rectangle(2.5, 3.5, 5.5, 5.5, color_purple, 1);
     
-    al_flip_display();
     
-    al_rest(10.0);
     
-    al_shutdown_primitives_addon();
     
-    al_destroy_display(display);
+    
+    
+   
 
     //BOOST----------------------------------------------------------------------------------
     
@@ -179,7 +263,7 @@ int main(int argc, char **argv)
     my_split.close();
     
     
-    
+    Deinitialize();
     
     
     
